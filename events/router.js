9047753b7a9ce530d429628d2c0e3bafee1b7965
sssgordon <gso7@uwo.ca>
2019-11-26 15:11:51 +0100

    Add endpoint to get all tickets of one event

--- events/router.js
+++ events/router.js
@@ -21,6 +21,7 @@ router.post("/events", async (request, response, next) => {
   }
 });
 
+// get all events
 router.get("/events", async (request, response, next) => {
   try {
     // console.log("DATE TEST", Date.now());
@@ -28,6 +29,19 @@ router.get("/events", async (request, response, next) => {
       { where: { date: { [Sequelize.Op.gte]: Date.now() } }, include: [Ticket] } // this returns only the dates gte today
     );
     response.send(events);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get all tickets of one event
+router.get("/events/:eventId/tickets", async (request, response, next) => {
+  try {
+    // console.log("DATE TEST", Date.now());
+    const event = await Event.findOne(
+      { where: { id: request.params.eventId }, include: [Ticket] } // this returns only the dates gte today
+    );
+    response.send(event.tickets);
   } catch (error) {
     next(error);
   }
